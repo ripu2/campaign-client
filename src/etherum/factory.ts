815b6d77b5e78e920fe82f4b13f19e6aa7ba5172
Ripudaman <44752831+ripu2@users.noreply.github.com>
2022-05-19 14:18:23 +0530

    env setu

--- src/etherum/factory.ts
+++ src/etherum/factory.ts
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json'
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0xa32e0d57E7C76950B61d0f5939810E174706dB51'
+    process.env.NEXT_PUBLIC_ADDRESS as string
 )
 
 export default instance
